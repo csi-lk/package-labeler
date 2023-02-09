7f36577cb9401cb6eabfc5c80a4518d37bc538e5
Jrson <45284565+jrson83@users.noreply.github.com>
2023-02-09 07:34:07 +0100

    fix: broken code after deps update

--- src/package-labeler.ts
+++ src/package-labeler.ts
@@ -1,74 +1,80 @@
-import { getInput, debug, error, setFailed } from "@actions/core"
-import * as github from "@actions/github"
-import multimatch from "multimatch"
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Octokit } from "@octokit/rest";
+import multimatch from "multimatch";
 
 async function run() {
   try {
-    const token = getInput("repo-token", { required: true })
-    const workspace = getInput("workspace", { required: true })
-    debug(`workspace: ${workspace}`)
-    const prefix = getInput("prefix", { required: false })
-    debug(`prefix: ${prefix}`)
-    const pullRequest = github.context.payload.pull_request
-    if (!pullRequest) {
-      error("could not find pull request number")
-      setFailed("could not find pull request number")
-      return
+    const token = core.getInput("repo-token", { required: true });
+    if (!token) {
+      core.error(
+        "failed to provide a GitHub token for accessing the GitHub REST API."
+      );
     }
-    const prNumber = pullRequest.number
-    const client = new github.GitHub(token)
-    debug(`fetching changed files for pr #${prNumber}`)
-    const changedFiles: string[] = await getChangedFiles(client, prNumber)
-    console.log(changedFiles)
-    const subDirs = (workspace.match(/\//g) || []).length
+    const workspace = core.getInput("workspace", { required: true });
+    core.debug(`workspace: ${workspace}`);
+    const prefix = core.getInput("prefix", { required: false });
+    core.debug(`prefix: ${prefix}`);
+    const pullRequest = github.context.payload.pull_request;
+    if (!pullRequest) {
+      core.error("could not find pull request number");
+      core.setFailed("could not find pull request number");
+      return;
+    }
+    const prNumber = pullRequest.number;
+    const client = new Octokit({
+      auth: token,
+    });
+    core.debug(`fetching changed files for pr #${prNumber}`);
+    const changedFiles: string[] = await getChangedFiles(client, prNumber);
+    console.log(changedFiles);
+    const subDirs = (workspace.match(/\//g) || []).length;
     const labels: string[] = multimatch(changedFiles, [
-      `${workspace}/**/*`
-    ]).map(dir => `${prefix}${dir.split("/")[1 + subDirs]}`)
+      `${workspace}/**/*`,
+    ]).map((dir) => `${prefix}${dir.split("/")[1 + subDirs]}`);
 
     if (labels.length > 0) {
-      await addLabels(client, prNumber, labels)
+      await addLabels(client, prNumber, labels);
     } else {
-      debug("no labels to add")
+      core.debug("no labels to add");
     }
-  } catch (err) {
-    error(err)
-    setFailed(err.message)
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      core.error(err);
+      core.setFailed(err.message);
+    }
   }
 }
 
 async function getChangedFiles(
-  client: github.GitHub,
+  client: Octokit,
   prNumber: number
 ): Promise<string[]> {
   const listFilesResponse = await client.pulls.listFiles({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    pull_number: prNumber
-  })
+    pull_number: prNumber,
+  });
 
-  const changedFiles = listFilesResponse.data.map(f => f.filename)
+  const changedFiles = listFilesResponse.data.map((f) => f.filename);
 
-  debug("found changed files:")
+  core.debug("found changed files:");
   for (const file of changedFiles) {
-    debug("  " + file)
+    core.debug("  " + file);
   }
 
-  return changedFiles
+  return changedFiles;
 }
 
-async function addLabels(
-  client: github.GitHub,
-  prNumber: number,
-  labels: string[]
-) {
-  debug(`labels to add: ${labels}`)
+async function addLabels(client: Octokit, prNumber: number, labels: string[]) {
+  core.debug(`labels to add: ${labels}`);
 
   await client.issues.addLabels({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     issue_number: prNumber,
-    labels: labels
-  })
+    labels: labels,
+  });
 }
 
-run()
+run();
